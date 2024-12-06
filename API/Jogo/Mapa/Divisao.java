ee9945b7030851d7162cac1b99701ea3146f6cf2
Arneiro <joao.a.carneiro@gmail.com>
2024-12-06 15:58:30 +0000

    +

--- API/Jogo/Mapa/Divisao.java
+++ API/Jogo/Mapa/Divisao.java
@@ -36,18 +36,18 @@ public class Divisao {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
         Divisao other = (Divisao) obj;
+        
+        // return Objects.equals(nome, other.nome); // Objects.equals: Substitui manualmente a verificação de null e a chamada ao método equals. Ele cuida disso internamente.
         if (nome == null) {
             if (other.nome != null)
                 return false;
         } else if (!nome.equals(other.nome))
             return false;
         return true;
+
     }
 }
--- API/Jogo/Mapa/Mapa.java
+++ API/Jogo/Mapa/Mapa.java
@@ -11,20 +11,18 @@ import Interfaces.List.UnorderedListADT;
 public class Mapa<T> extends ArrayGraph<T> {
 
     public Iterator<T> getAdjacentes(T vertex) {
-        int vertexIndex = getIndex(vertex);
+        int vertexIndex = getIndex(vertex); // não estava visivel
 
         // If the vertex is invalid, return an empty iterator
-        if (!indexIsValid(vertexIndex)) {
+        if (!indexIsValid(vertexIndex)) // a mesma coisa
             return new ArrayUnorderedList<T>().iterator();
-        }
 
         UnorderedListADT<T> adjacentVertices = new ArrayUnorderedList<>();
 
         // Find all adjacent vertices
         for (int i = 0; i < numVertices; i++) {
-            if (adjMatrix[vertexIndex][i] == 1) {
+            if (adjMatrix[vertexIndex][i] == 1)
                 adjacentVertices.addToRear(vertices[i]);
-            }
         }
 
         return adjacentVertices.iterator();
